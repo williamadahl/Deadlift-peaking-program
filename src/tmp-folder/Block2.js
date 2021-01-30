8d08f403e5f3e8bcec67f7f4bec8be48dd6685ac
William Dahl <williamad90@gmail.com>
2021-01-30 19:16:38 +0100

    moved exercise rendering into week child component

--- src/tmp-folder/Block2.js
+++ src/tmp-folder/Block2.js
@@ -5,7 +5,11 @@ import Week from './Week';
 
 
 const Block2 = () => {
+    const [exercises, setExercises] = useState(data[1]);
     const [someShit, setSomeShit] = useState(3);
+    const [weight, setWeight] = useState(420);
+    console.log(weight)
+
 
     const getData = (value) =>{
         console.log(value);
@@ -15,7 +19,7 @@ const Block2 = () => {
         <>
             <h1> Block 2 </h1>
             <h4> Lick my ass {someShit} times, slave</h4>
-            <Week getNumberOfLicks={setSomeShit}/>
+            <Week getNumberOfLicks={setSomeShit} exercises={exercises} weight={weight}/>
         </>
 
     )
--- src/tmp-folder/Week.js
+++ src/tmp-folder/Week.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
-const Week = (props) =>{
+const calculateWeight = (percentile,weight) => {
+    console.log(percentile, weight)
+    return (percentile*weight) /100;
+    // const weightToLift = exercise * weight;
+    // return weightToLift/100;
+};
+
+
+const Week = (props) => {
 
     const functionHandler = (timesLicked) => {
         props.getNumberOfLicks(timesLicked);
@@ -13,6 +21,16 @@ const Week = (props) =>{
                input='number'
                onChange={(e) =>functionHandler(parseInt(e.target.value))}
             />
+            {props.exercises.map((exercise) => {
+                const {id, fields} = exercise;
+                return (
+                    <div key = {id} className='item'>
+                        <h4>{fields.name}</h4>
+                        <p>Sets/Reps: {fields.sets} x {fields.reps}</p>
+                        <p>Weight: {calculateWeight(fields.percentile, props.weight)}</p>
+                    </div>
+                );
+            })}
             <h5>Something after input</h5>
         </>
     )
