12771c2aa4ccb0345c0b1cd4d1395cc2e51641ee
William Dahl <williamad90@gmail.com>
2021-01-30 16:34:43 +0100

    found way to use short circuit logic to toggle exercises for a week

--- src/tmp-folder/Block1.js
+++ src/tmp-folder/Block1.js
@@ -6,26 +6,31 @@ import {data} from '../data/data'
 
 const Block1 = (props) => {
     const [exercises, setExercises] = useState(data[0]);
-
+    const [name, setName] = useState('');
+    const [showWeekOne, setShowWeekOne] = useState(false);
+    console.log(showWeekOne)
 
     return(
         <>
             <h1> Block 1 </h1>
+            <button className='btn' onClick={()=>setShowWeekOne(!showWeekOne)}> Week 1 toggle</button>
+            {showWeekOne && <>
             <input
                 type='text'
                 placeholder='hello from block1'
-                onChange={event => props.onChange(event.target.value)}
+                onChange={event => props.onChange(event.target.value) && setName(event.target.value)}
+                // onChange={setName('test')}
             />
             {exercises.map((exercise) => {
                 const {id, fields} = exercise;
                 return (
-                    <div key = {id} className='item'>
-                        <h4>{fields.name}</h4>
-                        <p>Sets/Reps: {fields.sets} x {fields.reps}</p>
-                    </div>
+                <div key = {id} className='item'>
+                <h4>{fields.name}</h4>
+                <p>Sets/Reps: {fields.sets} x {fields.reps}</p>
+                </div>
                 );
             })}
-
+            </>}
 
         </>
     );
--- src/tmp-folder/Home.js
+++ src/tmp-folder/Home.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react';
 import Block1 from "./Block1";
 
 const Home = () => {
-    const [onChangeValue, setOnChangeValue] = useState(0);
+    // const [onChangeValue, setOnChangeValue] = useState(0);
     const [message, setMessage] = useState('I am home');
 
 
