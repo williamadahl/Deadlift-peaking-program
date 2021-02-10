161eefa957327c0c6d9fa51fba061fccbbc3d769
William Dahl <williamad90@gmail.com>
2021-02-10 19:43:23 +0100

    pulled out the workout into a seperate component as well

--- src/tmp-folder/Block4.js
+++ src/tmp-folder/Block4.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import {data} from '../data/data';
 import {useDispatch} from "react-redux";
 import {useSelector} from "react-redux";
-import Results from './Results'
 
 
 const Block4 = () => {
@@ -10,44 +9,56 @@ const Block4 = () => {
     const selectNewMax = state => state.newAchievedMax;
     const achievedMax = useSelector(selectNewMax);
     const [exercises] = useState(data[3]);
-    const [submittedMax, setSubmittedMax] = useState('')
+    const [submittedMax, setSubmittedMax] = useState('');
+
+    const [showWorkout, setShowWorkout] = useState(true);
     const [showResults, setShowResults] = useState(false);
 
     const handleSubmit = (e) => {
         dispatch({type:'SET_NEW_MAX', payload:submittedMax})
         e.preventDefault()
     }
-    const Test = () => {
+    const WorkoutComponent = () => {
         return (
-            <h1>TEST</h1>
+            <>
+                <h1> Max attempt</h1>
+                <h5>This is achieved max :{achievedMax}</h5>
+                {exercises.map((exercise) => {
+                const{id, fields} = exercise;
+                    return (
+                        <div key={id} className='item'>
+                        <h4>{fields.name}</h4>
+                        <p>Sets/Reps: {fields.sets} x {fields.reps}</p>
+                        </div>
+                    )
+                })}
+                <form className='form' onSubmit={handleSubmit}>
+                    <input
+                    type='number'
+                    value={submittedMax}
+                    onChange={(e ) => setSubmittedMax(e.target.value)}
+                />
+                <button type='submit'>Submit new Max</button>
+                </form>
+            </>
+                )
+    }
+
+    const ResultsComponent = () => {
+        return (
+            <>
+                <h3> Congratulations on completing this peaking program</h3>
+                <h5>Previous best: {previousMax}</h5>
+                <h5>New best: {newMax}</h5>
+                <h5>That is an increase of: {newMax-previousMax}</h5>
+            </>
         )
     }
 
     return (
         <>
-            {showResults &&
-            <Results/>
-            }
-            {<Test/>}
-            <h1> Max attempt</h1>
-            <h5>This is achieved max :{achievedMax}</h5>
-            {exercises.map((exercise) => {
-                const{id, fields} = exercise;
-                return (
-                <div key={id} className='item'>
-                    <h4>{fields.name}</h4>
-                    <p>Sets/Reps: {fields.sets} x {fields.reps}</p>
-                </div>
-            )
-            })}
-            <form className='form' onSubmit={handleSubmit}>
-                <input
-                    type='number'
-                    value={submittedMax}
-                    onChange={(e ) => setSubmittedMax(e.target.value)}
-                />
-                    <button type='submit'>Submit new Max</button>
-            </form>
+            {<ResultsComponent/>}
+            {<WorkoutComponent/>}
         </>
 
     )
